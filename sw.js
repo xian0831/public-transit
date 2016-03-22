c83d2a07366302a3d8aaae777b537a236ef2b06b
xian0831 <xian0831@gmail.com>
2016-03-21 22:10:06 -0400

    Disable: disable service worker code for now

--- sw.js
+++ sw.js
@@ -1,21 +1,25 @@
-/**
- * Created by andrew on 3/15/16.
- */
-'use strict';
-
-this.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('v1').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/index.html'
-            ]);
-        })
-    );
-});
-
-this.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-    );
-});
+///**
+// * Created by andrew on 3/15/16.
+// */
+//'use strict';
+//
+//this.addEventListener('install', function(event) {
+//    event.waitUntil(
+//        caches.open('v1').then(function(cache) {
+//            return cache.addAll([
+//                '/',
+//                '/index.html',
+//                '/data/agency.json'
+//
+//            ]);
+//        })
+//    );
+//});
+//
+//this.addEventListener('fetch', function(event) {
+//    event.respondWith(
+//        caches.match(event.request).catch(function() {
+//            return fetch(event.request);
+//        })
+//    );
+//});
