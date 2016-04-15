689e0640acf4b4873eb8749f257fdaa2699b58ae
xian0831 <xian0831@gmail.com>
2016-04-14 22:35:30 -0400

    Add: constant for DC Metro API Key

--- app/app.module.js
+++ app/app.module.js
@@ -3,4 +3,8 @@
 
     angular
         .module('transit-app', ['ui.router']);
+
+    angular
+        .modeule('transit-app')
+        .constant('WMATA_API_KEY','6b700f7ea9db408e9745c207da7ca827');
 })();
