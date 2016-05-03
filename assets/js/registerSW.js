08f166f06e2b795fe07f51fdf273cc4973192d23
xian0831 <xian0831@gmail.com>
2016-05-02 21:21:39 -0400

    Add: service worker for offline development

--- assets/js/registerSW.js
+++ assets/js/registerSW.js
@@ -1,15 +1,15 @@
-///**
-// * Created by andrew on 3/15/16.
-// */
-//(function(){
-//    'use strict';
-//    if ('serviceWorker' in navigator) {
-//        navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(function(reg) {
-//            // registration worked
-//            console.log('Registration succeeded. Scope is ' + reg.scope);
-//        }).catch(function(error) {
-//            // registration failed
-//            console.log('Registration failed with ' + error);
-//        });
-//    }
-//})();
+/**
+ * Created by andrew on 3/15/16.
+ */
+(function(){
+    'use strict';
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(function(reg) {
+            // registration worked
+            console.log('Registration succeeded. Scope is ' + reg.scope);
+        }).catch(function(error) {
+            // registration failed
+            console.log('Registration failed with ' + error);
+        });
+    }
+})();
