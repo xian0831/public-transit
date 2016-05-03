f218689d47e3ecae077316e5103ac92254fdc790
xian0831 <xian0831@gmail.com>
2016-05-02 21:20:39 -0400

    Add: service worker to handle offline and lifi

--- sw.js
+++ sw.js
@@ -1,25 +1,40 @@
 ///**
 // * Created by andrew on 3/15/16.
 // */
-//'use strict';
-//
-//this.addEventListener('install', function(event) {
-//    event.waitUntil(
-//        caches.open('v1').then(function(cache) {
-//            return cache.addAll([
-//                '/',
-//                '/index.html',
-//                '/data/agency.json'
-//
-//            ]);
-//        })
-//    );
-//});
-//
-//this.addEventListener('fetch', function(event) {
-//    event.respondWith(
-//        caches.match(event.request).catch(function() {
-//            return fetch(event.request);
-//        })
-//    );
-//});
+'use strict';
+
+this.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open('my-transit-cache-v1').then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                'assets/vendor/js/angular.js',
+                'https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.18/angular-ui-router.js',
+                'assets/js/all.min.js',
+                '//code.jquery.com/jquery-1.12.0.min.js',
+                'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/4.1.2/papaparse.min.js',
+                'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',
+                'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js',
+                'app/partials/home/home.html',
+                'data/rail_stops.txt',
+                'data/rail_stop_time.txt'
+
+            ]);
+        })
+    );
+});
+
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                    // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
